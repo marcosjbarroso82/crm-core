19aef149b5cca1178682165cccfc561bc7b8d16d
Marcos Javier Barroso <marcosjbarroso82@gmail.com>
2024-08-21 09:55:07 -0300

    Customer (#4)
    * test CI
    
    * test CI
    
    * test dummy
    
    * fixed indentation
    
    * implemented customer endpoints
    
    * test users endpoint
    
    * customer tests
    
    * customer tests
    
    * refactor
    
    * refactor
    
    * fixed photo url
    
    * fixed tests
    
    * set password
    
    * documentation
    
    * linting

--- crm_core/user/api/v1/views.py
+++ crm_core/user/api/v1/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from crm_core.user import models
@@ -8,3 +12,14 @@ class UserViewSet(ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = UserSerializer
     filterset_fields = ["username", "email", "first_name", "last_name"]
+
+    @action(detail=True, methods=['post'], url_path='set-password')
+    def set_password(self, request, pk=None):
+        user = self.get_object()
+        password = request.data.get('password')
+        if not password:
+            return Response({'error': 'Password is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.password = make_password(password)
+        user.save()
+        return Response({'status': 'password set'}, status=status.HTTP_200_OK)

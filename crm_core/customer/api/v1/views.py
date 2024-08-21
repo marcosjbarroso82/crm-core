c9374dc0d95bbecdc14d9a358a36598d2099d362
Marcos <marcosjbarroso82@gmail.com>
2024-08-21 12:18:31 +0000

    set password

--- crm_core/customer/api/v1/views.py
+++ crm_core/customer/api/v1/views.py
@@ -1,4 +1,5 @@
 from django.http import FileResponse
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
